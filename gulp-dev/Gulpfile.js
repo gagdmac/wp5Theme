293ec1381517d1222d8667c2a62f08a1a9e35e73
Gabriel <gabriel@gagdfulsatackdeveloper.com>
2019-04-23 00:19:58 +0200

    gulp and browserSync  working

--- gulp-dev/Gulpfile.js
+++ gulp-dev/Gulpfile.js
@@ -58,7 +58,7 @@ gulp.task('javascript', function() {
 gulp.task('watch', function() {
 	browserSync.init({ 
 		open: 'external',
-		proxy: 'humescores.dev',
+		proxy: 'http://localhost/WP/firstTheme/',
 		port: 8080
 	});
 	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']);
